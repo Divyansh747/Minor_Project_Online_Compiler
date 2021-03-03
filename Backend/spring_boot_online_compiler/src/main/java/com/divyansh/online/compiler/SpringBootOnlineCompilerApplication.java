cda4fc2ec00eb06330a8ddc0580196980f4fbd7a
divyansh747 <divyansh.1999.dr@gmail.com>
2021-03-03 20:12:49 +0530

    spring boot starter project

---
+++ Backend/spring_boot_online_compiler/src/main/java/com/divyansh/online/compiler/SpringBootOnlineCompilerApplication.java
@@ -0,0 +1,13 @@
+package com.divyansh.online.compiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnlineCompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnlineCompilerApplication.class, args);
+	}
+
+}
---
+++ Backend/spring_boot_online_compiler/src/test/java/com/divyansh/online/compiler/SpringBootOnlineCompilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyansh.online.compiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnlineCompilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
