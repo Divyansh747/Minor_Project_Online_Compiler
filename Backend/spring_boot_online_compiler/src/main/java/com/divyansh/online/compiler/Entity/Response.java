a9b8f78348b99f7a7c2588b0852c74adf2f34680
divyansh747 <divyansh.1999.dr@gmail.com>
2021-03-08 20:38:39 +0530

    add Response Entity

--- Backend/spring_boot_online_compiler/src/main/java/com/divyansh/online/compiler/Entity/Response.java
+++ Backend/spring_boot_online_compiler/src/main/java/com/divyansh/online/compiler/Entity/Response.java
@@ -1,5 +1,54 @@
 package com.divyansh.online.compiler.Entity;
 
-public class Response {
+import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+
+@Entity
+public class Response {
+	private String output;
+	private String requiredoutput;
+	private String statuscode;
+	private LocalDateTime ldt;
+	
+	public Response(String output, String requiredoutput, String statuscode, LocalDateTime ldt) {
+		super();
+		this.output = output;
+		this.requiredoutput = requiredoutput;
+		this.statuscode = statuscode;
+		this.ldt = ldt;
+	}
+
+	public String getOutput() {
+		return output;
+	}
+
+	public void setOutput(String output) {
+		this.output = output;
+	}
+
+	public String getRequiredoutput() {
+		return requiredoutput;
+	}
+
+	public void setRequiredoutput(String requiredoutput) {
+		this.requiredoutput = requiredoutput;
+	}
+
+	public String getStatuscode() {
+		return statuscode;
+	}
+
+	public void setStatuscode(String statuscode) {
+		this.statuscode = statuscode;
+	}
+
+	public LocalDateTime getLdt() {
+		return ldt;
+	}
+
+	public void setLdt(LocalDateTime ldt) {
+		this.ldt = ldt;
+	} 
+	
 }
